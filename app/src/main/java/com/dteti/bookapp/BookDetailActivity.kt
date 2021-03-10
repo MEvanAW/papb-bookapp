f883a3156bfda056d811fd93f9ad516b71401484
MEvanAW <evananindya@mail.ugm.ac.id>
2021-03-10 09:24:06 +0700

    Menambahkan Book Detail Activity dan Reading Activity.

---
+++ app/src/main/java/com/dteti/bookapp/BookDetailActivity.kt
@@ -0,0 +1,11 @@
+package com.dteti.bookapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BookDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_book_detail)
+    }
+}
