1f31ee5e80ef6d66671275b2f4a748ca31586b8f
MEvanAW <evananindya@mail.ugm.ac.id>
2021-03-18 09:03:55 +0700

    Rename Book Topic Fragment Parameter

--- app/src/main/java/com/dteti/bookapp/BookTopicFragment.kt
+++ app/src/main/java/com/dteti/bookapp/BookTopicFragment.kt
@@ -6,10 +6,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+// The fragment initialization parameter(s)
+private const val ARG_TOPIC = "Topic"
 
 /**
  * A simple [Fragment] subclass.
@@ -17,15 +15,12 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class BookTopicFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private var topic: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+            topic = it.getString(ARG_TOPIC)
         }
     }
 
@@ -46,11 +41,10 @@ class BookTopicFragment : Fragment() {
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
-        fun newInstance(param1: String, param2: String) =
+        fun newInstance(topic: String) =
                 BookTopicFragment().apply {
                     arguments = Bundle().apply {
-                        putString(ARG_PARAM1, param1)
-                        putString(ARG_PARAM2, param2)
+                        putString(ARG_TOPIC, topic)
                     }
                 }
     }
