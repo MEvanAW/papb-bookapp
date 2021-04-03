d483e1b7072b6e15b230fd8190b9e2e94fd33c2c
M. Evan Anindya W <evananindya@mail.ugm.ac.id>
2021-04-03 18:41:43 +0700

    Add data binding to Reading Activity (#9)
    * Add data binding for Reading Activity.

--- app/src/main/java/com/dteti/bookapp/view/ui/activities/ReadingActivity.kt
+++ app/src/main/java/com/dteti/bookapp/view/ui/activities/ReadingActivity.kt
@@ -2,22 +2,19 @@ package com.dteti.bookapp.view.ui.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
+import androidx.databinding.DataBindingUtil
 import com.dteti.bookapp.R
+import com.dteti.bookapp.databinding.ActivityReadingBinding
 
-class ReadingActivity : AppCompatActivity(), View.OnClickListener {
+class ReadingActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_reading)
 
+        // Data binding
+        val binding: ActivityReadingBinding = DataBindingUtil.setContentView(this, R.layout.activity_reading)
         // Digunakan untuk kembali ke activity sebelumnya
-        val iv_back: android.widget.ImageView = findViewById(R.id.iv_back)
-        iv_back.setOnClickListener(this)
-    }
-
-    override fun onClick(v: View?) {
-        when(v?.id){
-            R.id.iv_back -> finish()
-        }
+        binding.ivBack.setOnClickListener{ super.onBackPressed() }
     }
 }
